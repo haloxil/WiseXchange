5b1ea38df7ae77b76e4c752ee08cae912e8d9851
Sean Evan Seah <seanevanseah@gmail.com>
2022-08-26 22:51:59 +0800

    Vibes

---
+++ WiseXchange/src/main/java/com/example/WiseXchange/WiseXchangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.WiseXchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseXchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiseXchangeApplication.class, args);
+	}
+
+}
---
+++ WiseXchange/src/test/java/com/example/WiseXchange/WiseXchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WiseXchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseXchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
