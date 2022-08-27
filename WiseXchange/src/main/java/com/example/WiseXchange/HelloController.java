ced34cf03e2c806a954782cdf05057d483703e3b
Sean Evan Seah <seanevanseah@gmail.com>
2022-08-27 17:21:37 +0800

    Yay we have used an API

--- WiseXchange/src/main/java/com/example/WiseXchange/HelloController.java
+++ WiseXchange/src/main/java/com/example/WiseXchange/HelloController.java
@@ -1,43 +1,36 @@
 package com.example.WiseXchange;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/index")
-    public String index() {
-        try {
-            URL url = new URL(
-                    "https://www.dbs.com/sandbox/api/sg/v1/rates/historicalExchangeRates?quoteCurrency=USD&fromDate=2027-08-31&toDate=2027-08-31&boardRateType=On%20Demand");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setDoOutput(true);
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.setRequestProperty("clientId", "clientId1");
-            conn.setRequestProperty("accessToken",
-                    "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiIDogImNhcGkuZGJzLmNvbSIsInB0eXR5cGUiIDogIjMiLCJjbG50eXBlIiA6ICIyIiwiY2xuaWQiIDogImNsaWVudElkMSIsIlBhcnR5SWQiIDogIlltRmllUT09IiwgImFjY2VzcyIgOiAiMUZBIiwic2NvcGUiIDogIlJFQUQiICwiYXVkIiA6ICJjYXBpLmRicy5jb20vYWNjZXNzIiAsImlhdCIgOiAiVHVlIEF1ZyAwOCAxNDo0MDo1NyBTR1QgMjAxNyIsICJleHAiIDogIjE1MDIxODUyNTc5ODYifQ.cWTXScPzTJXyHZdMVJ_OvffDJBUJHqlfRq-6khf70cE");
-            if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode() + " "
-                        + conn.getErrorStream() + " " + conn.getResponseMessage());
-            }
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));) {
-                String output;
-                while ((output = br.readLine()) != null) {
-                    System.out.println(output);
-                }
-            }
-            conn.disconnect();
-        } catch (Exception e) {
-            System.out.println("Unable to call API." + e.getMessage());
-        }
+    public String index() throws IOException, JSONException {
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
 
-        return "Hello";
-    }
+        Request request = new Request.Builder()
+                .url("https://api.apilayer.com/exchangerates_data/convert?to=SGD&from=EUR&amount=5")
+                .addHeader("apikey", "HSjaNL3rJldGusO7sMstFOJh3tQRdOmL")
+                .get().build();
+
+    Response response = client.newCall(request).execute();
+    String jsonData = response.body().string();
+    JSONObject Jobject = new JSONObject(jsonData);
+    String Date = Jobject.getString("date");
+    Double rate = Jobject.getJSONObject("info").getDouble("rate");
+    Double result = Jobject.getDouble("result");
+
+    return Date + ": Converting 5 EUR to SGD has a rate of " + rate + " which gives you " + result;
+}
 
     @GetMapping("/login")
     public String login() {
