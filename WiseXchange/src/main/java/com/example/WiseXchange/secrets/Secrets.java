cde9f2a51525c571e4900312ac6409acfba85010
Sean Evan Seah <dance0511@gmail.com>
2022-08-27 15:22:18 +0800

    Delete WiseXchange/src/main/java/com/example/WiseXchange/secrets directory

--- WiseXchange/src/main/java/com/example/WiseXchange/secrets/Secrets.java
+++
@@ -1,9 +0,0 @@
-package com.example.WiseXchange.secrets;
-
-public class Secrets {
-    static String apiKey = "26d185bb601bc656f1db97e7df742d43";
-
-    public static String getApiKey() {
-        return apiKey;
-    }
-}
