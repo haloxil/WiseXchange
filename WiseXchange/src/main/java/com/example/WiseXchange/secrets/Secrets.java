bc35a12d61fc4b809d0247130897ba8bf6f4c233
Sean Evan Seah <seanevanseah@gmail.com>
2022-08-27 15:18:55 +0800

    Api Key was visible

---
+++ WiseXchange/src/main/java/com/example/WiseXchange/secrets/Secrets.java
@@ -0,0 +1,9 @@
+package com.example.WiseXchange.secrets;
+
+public class Secrets {
+    static String apiKey = "26d185bb601bc656f1db97e7df742d43";
+
+    public static String getApiKey() {
+        return apiKey;
+    }
+}
